31178d793059f93c07cefeaa47dc97837177eadd
rizavelioglu <rzavelioglu@gmail.com>
2023-08-28 10:27:38 +0200

    unify evaluation across datasets

--- src/fashionfail/models/evaluate.py
+++ src/fashionfail/models/evaluate.py
@@ -1,4 +1,3 @@
-import pickle
 from pprint import pprint
 
 import numpy as np
@@ -11,12 +10,8 @@ from torchmetrics.detection.mean_ap import MeanAveragePrecision
 from torchvision.ops import box_convert
 from tqdm import tqdm
 
-from fashionfail.process_preds import (
-    clean_df_preds,
-    convert_tpu_preds_to_coco,
-    load_tpu_preds,
-)
-from fashionfail.utils import load_categories, yxyx_to_xyxy
+from fashionfail.process_preds import convert_tpu_preds_to_coco, load_tpu_preds
+from fashionfail.utils import yxyx_to_xyxy
 
 
 def get_cli_args():
@@ -42,98 +37,17 @@ def get_cli_args():
         default="bbox",
     )
     parser.add_argument(
-        "--dataset",
+        "--eval_method",
         type=str,
         required=True,
-        choices=[
-            "fashionpedia",
-            "fashionpedia-coco",
-            "fashionfail",
-            "fashionfail-coco",
-        ],
-        help="Name of the dataset for which evaluation should be executed.",
+        choices=["COCO", "TorchMetrics", "all"],
+        help="The name of the evaluation framework to be used, or `all` to run all eval methods.",
     )
 
     return parser.parse_args()
 
 
-def eval_on_fashionpedia(args):
-    # Define constants
-    catIds = list(range(0, 28))
-
-    # Load GT annotations
-    coco = COCO(args.anns_path)
-
-    # Load predictions from disk
-    df_preds = load_tpu_preds(cli_args.preds_path)
-    df_preds = clean_df_preds(df_preds)
-
-    # Initialize metric
-    metric = MeanAveragePrecision(
-        class_metrics=True, box_format="xywh", iou_type=args.iou_type
-    )
-
-    for i in tqdm(
-        coco.imgs.values(),
-        desc="Accumulating predictions and targets",
-    ):
-        img_id, img_name = i["id"], i["file_name"]
-
-        # Get Ground-Truth for the image
-        annIds = coco.getAnnIds(imgIds=img_id, catIds=catIds, iscrowd=None)
-        anns = coco.loadAnns(annIds)
-        gt = pd.DataFrame(anns)
-        # Sanity check whether annotation exists for each image
-        assert (
-            not gt.empty
-        ), f"No annotation found in the annotation file for image: {img_name} with image ID: {img_id}"
-        gt = gt.groupby("image_id").agg(
-            {"category_id": list, "segmentation": list, "bbox": list}
-        )
-        # Parse GT
-        gt_box = np.array(gt.bbox.values[0])
-        gt_class = np.array(gt.category_id.values[0]) + 1
-
-        # Get detections for the image
-        dt = df_preds[df_preds.image_file == img_name]
-        # Sanity check whether annotation exists for each image
-        assert (
-            not dt.empty
-        ), f"No prediction found in predictions file for image: {img_name} with image ID: {img_id}"
-        # Parse DT
-        dt_boxes = box_convert(
-            yxyx_to_xyxy(torch.tensor(dt["boxes"].values[0])),
-            in_fmt="xyxy",
-            out_fmt="xywh",
-        )
-        dt_scores = dt["scores"].values[0]
-        dt_classes = dt["classes"].values[0]
-
-        preds = [
-            dict(
-                boxes=dt_boxes,
-                scores=torch.tensor(dt_scores),
-                labels=torch.tensor(dt_classes),
-            )
-        ]
-
-        target = [
-            dict(
-                boxes=torch.tensor(gt_box),
-                labels=torch.tensor(gt_class),
-            )
-        ]
-
-        # Update metric with predictions and respective ground truth
-        metric.update(preds, target)
-
-    logger.info("Computing evaluation metrics...")
-    # Compute the results
-    result = metric.compute()
-    pprint(result)
-
-
-def eval_on_fashionpedia_coco(args):
+def eval_with_coco(args):
     # Convert AMRCNN predictions to COCO format
     annotation_file = convert_tpu_preds_to_coco(
         preds_path=args.preds_path, anns_path=args.anns_path
@@ -153,53 +67,60 @@ def eval_on_fashionpedia_coco(args):
     coco_eval.summarize()
 
 
-def eval_on_fashionfail(args):
-    # Load predictions from disk
-    df_preds = load_tpu_preds(cli_args.preds_path)
-    df_preds = clean_df_preds(df_preds)
+def eval_with_torchmetrics(args):
+    # Load GT annotations
+    coco = COCO(args.anns_path)
 
-    # Parse Fashionpedia categories.
-    category_id_to_name = load_categories()
-    name_to_category_id = {v: k for k, v in category_id_to_name.items()}
-
-    GT_SAVE_DIR = "/home/rizavelioglu/work/data/fashionfail/annotations"
-
-    # Load label-GT
-    df_cat = pd.read_csv(
-        "/home/rizavelioglu/work/data/fashionfail/df_cat-processed.csv",
-        usecols=["image_name", "label"],
-    )
-    # Convert label to int id and create a new column "label_id"
-    df_cat["label_id"] = df_cat["label"].map(
-        lambda label: name_to_category_id.get(label)
-    )
+    # Load predictions
+    df_preds = load_tpu_preds(args.preds_path, preprocess=True)
 
     # Initialize metric
     metric = MeanAveragePrecision(
         class_metrics=True, box_format="xywh", iou_type=args.iou_type
     )
 
-    for i, row in tqdm(
-        df_preds.iterrows(),
-        total=df_preds.shape[0],
-        desc="Accumulating predictions and targets",
+    catIds = list(range(0, 28))
+
+    for img_info in tqdm(
+        coco.imgs.values(), desc="Accumulating predictions and targets"
     ):
-        image_name = row["image_file"].replace(".jpg", "")
+        img_id, img_name = img_info["id"], img_info["file_name"]
 
-        # Load box-GT
-        with open(f"{GT_SAVE_DIR}/{image_name}.pkl", "rb") as f:
-            gt = pickle.load(f)
-        gt_box = torch.tensor(gt.xyxy)
+        # Get Ground-Truth for the image
+        annIds = coco.getAnnIds(imgIds=img_id, catIds=catIds, iscrowd=None)
+        anns = coco.loadAnns(annIds)
+        gt_df = pd.DataFrame(anns)
 
-        # Load class-GT
-        gt_class = (
-            df_cat[df_cat.image_name == f"{image_name}.jpg"].label_id.values[0] + 1
-        )
+        if gt_df.empty:
+            raise AssertionError(
+                f"No annotation found for image: {img_name} with image ID: {img_id}"
+            )
+        # Check if there are multiple ground truths, if so aggregate them
+        if gt_df.shape[0] > 1:
+            gt_df = gt_df.groupby("image_id").agg(
+                {"category_id": list, "segmentation": list, "bbox": list}
+            )
 
-        # Get prediction for the image
-        dt_boxes = yxyx_to_xyxy(torch.tensor(row["boxes"]))
-        dt_classes = row["classes"]
-        dt_scores = row["scores"]
+        gt_box = np.array(gt_df["bbox"].values[0])
+        gt_class = np.array(gt_df["category_id"].values[0]) + 1
+
+        # Get detections for the image
+        dt_df = df_preds[df_preds.image_file == img_name]
+        if dt_df.empty:
+            raise AssertionError(
+                f"No prediction found for image: {img_name} with image ID: {img_id}"
+            )
+        # Handle the case where no detection is predicted
+        if dt_df["boxes"].values[0].size == 0:
+            dt_boxes = torch.tensor([])
+        else:
+            dt_boxes = box_convert(
+                yxyx_to_xyxy(torch.tensor(dt_df["boxes"].values[0])),
+                in_fmt="xyxy",
+                out_fmt="xywh",
+            )
+        dt_scores = dt_df["scores"].values[0]
+        dt_classes = dt_df["classes"].values[0]
 
         preds = [
             dict(
@@ -211,8 +132,12 @@ def eval_on_fashionfail(args):
 
         target = [
             dict(
-                boxes=gt_box,
-                labels=torch.tensor([gt_class]),
+                boxes=torch.unsqueeze(torch.tensor(gt_box), dim=0)
+                if gt_box.ndim == 1
+                else torch.tensor(gt_box),
+                labels=torch.tensor([gt_class])
+                if gt_class.size == 1
+                else torch.tensor(gt_class),
             )
         ]
 
@@ -224,25 +149,17 @@ def eval_on_fashionfail(args):
     result = metric.compute()
     pprint(result)
 
-    # Display per class metrics.
-    # cats = pd.read_json("/home/rizavelioglu/recommendy/repos/segmentation/segmentation/visualization/categories.json")[["name"]]
-    # cats["AP"] = result["map_per_class"].tolist()
-    # display(cats.sort_values('name').transpose())
-
-
-def eval_on_fashionfail_coco(args):
-    pass
-
 
 if __name__ == "__main__":
     cli_args = get_cli_args()
 
-    match cli_args.dataset:
-        case "fashionpedia":
-            eval_on_fashionpedia(cli_args)
-        case "fashionpedia-coco":
-            eval_on_fashionpedia_coco(cli_args)
-        case "fashionfail":
-            eval_on_fashionfail(cli_args)
-        case "fashionfail-coco":
-            eval_on_fashionfail_coco(cli_args)
+    match cli_args.eval_method:
+        case "COCO":
+            eval_with_coco(cli_args)
+        case "TorchMetrics":
+            eval_with_torchmetrics(cli_args)
+        case "all":
+            logger.info("=" * 10 + "Evaluating with COCOeval" + "=" * 10)
+            eval_with_coco(cli_args)
+            logger.info("=" * 10 + "Evaluating with TorchMetrics" + "=" * 10)
+            eval_with_torchmetrics(cli_args)
