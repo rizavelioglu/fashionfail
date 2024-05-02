bd84caef4d75acf31cbc5443841c1467b0f6de52
rizavelioglu <rzavelioglu@gmail.com>
2024-05-02 16:43:09 +0200

    update page

--- static/js/index.js
+++ static/js/index.js
@@ -2,40 +2,30 @@ window.HELP_IMPROVE_VIDEOJS = false;
 
 
 $(document).ready(function() {
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-
-    });
 
     var options = {
 			slidesToScroll: 1,
-			slidesToShow: 3,
+			slidesToShow: 1,
 			loop: true,
 			infinite: true,
 			autoplay: true,
-			autoplaySpeed: 3000,
+			autoplaySpeed: 5000,
     }
 
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
+	// Initialize all div with teaser-carousel class
+    var carousels = bulmaCarousel.attach('.teaser-carousel', options);
 
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
 
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
+    var options = {
+			slidesToScroll: 1,
+			slidesToShow: 1,
+			loop: true,
+			infinite: true,
+			autoplay: true,
+			autoplaySpeed: 10000,
     }
+	// Initialize all div with scale-carousel class
+    var carousels = bulmaCarousel.attach('.scale-carousel', options);
+
+
 })
